f16a0c445abec8019235e6cf1616b4d51491096c
umit219 <157153442+umit219@users.noreply.github.com>
2026-09-11 01:41:59 +0300

    fleet.js

--- fleet.js
+++ fleet.js
@@ -1,252 +1,393 @@
-const fleetData = [
+const fleet = [
 
-    {
-        registration: "TC-JIO",
-        aircraft: "Airbus A330-223",
-        family: "A330-200",
-        engine: "PW4168A",
-        manufacturer: "Pratt & Whitney"
-    },
+  // =========================
+  // A330-200
+  // =========================
 
-    {
-        registration: "TC-JIP",
-        aircraft: "Airbus A330-223",
-        family: "A330-200",
-        engine: "PW4168A",
-        manufacturer: "Pratt & Whitney"
-    },
+  {
+    registration: "TC-JIO",
+    aircraft: "A330-223",
+    family: "A330-200",
+    engine: "PW4168A",
+    manufacturer: "Pratt & Whitney",
+    name: "Eskişehir"
+  },
+  {
+    registration: "TC-JIP",
+    aircraft: "A330-223",
+    family: "A330-200",
+    engine: "PW4168A",
+    manufacturer: "Pratt & Whitney",
+    name: "Lale"
+  },
+  {
+    registration: "TC-JIT",
+    aircraft: "A330-223",
+    family: "A330-200",
+    engine: "PW4168A",
+    manufacturer: "Pratt & Whitney",
+    name: null
+  },
+  {
+    registration: "TC-JNA",
+    aircraft: "A330-203",
+    family: "A330-200",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Gaziantep"
+  },
+  {
+    registration: "TC-JNB",
+    aircraft: "A330-203",
+    family: "A330-200",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Konya"
+  },
+  {
+    registration: "TC-JNC",
+    aircraft: "A330-203",
+    family: "A330-200",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Kushimoto"
+  },
+  {
+    registration: "TC-JND",
+    aircraft: "A330-203",
+    family: "A330-200",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Antalya"
+  },
+  {
+    registration: "TC-JNE",
+    aircraft: "A330-203",
+    family: "A330-200",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Kayseri"
+  },
+  {
+    registration: "TC-LOH",
+    aircraft: "A330-223",
+    family: "A330-200",
+    engine: "PW4168A",
+    manufacturer: "Pratt & Whitney",
+    name: null
+  },
+  {
+    registration: "TC-LOM",
+    aircraft: "A330-243",
+    family: "A330-200",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: null
+  },
 
-    {
-        registration: "TC-JIT",
-        aircraft: "Airbus A330-223",
-        family: "A330-200",
-        engine: "PW4168A",
-        manufacturer: "Pratt & Whitney"
-    },
+  // =========================
+  // A330-300
+  // =========================
 
-    {
-        registration: "TC-JNA",
-        aircraft: "Airbus A330-203",
-        family: "A330-200",
-        engine: "CF6-80E1A3",
-        manufacturer: "General Electric"
-    },
+  {
+    registration: "TC-JNH",
+    aircraft: "A330-343",
+    family: "A330-300",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: "Topkapı"
+  },
+  {
+    registration: "TC-JNI",
+    aircraft: "A330-343",
+    family: "A330-300",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: "Konak"
+  },
+  {
+    registration: "TC-JNJ",
+    aircraft: "A330-343",
+    family: "A330-300",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: "Kapadokya"
+  },
+  {
+    registration: "TC-JNK",
+    aircraft: "A330-343",
+    family: "A330-300",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: "Şanlıurfa"
+  },
+  {
+    registration: "TC-JNL",
+    aircraft: "A330-343",
+    family: "A330-300",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: "Trabzon"
+  },
+  {
+    registration: "TC-JNM",
+    aircraft: "A330-343",
+    family: "A330-300",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: "Samsun"
+  },
+  {
+    registration: "TC-JNN",
+    aircraft: "A330-343",
+    family: "A330-300",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: "Selçuklu"
+  },
+  {
+    registration: "TC-JNO",
+    aircraft: "A330-343",
+    family: "A330-300",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: "Boğaziçi"
+  },
+  {
+    registration: "TC-JNP",
+    aircraft: "A330-343",
+    family: "A330-300",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: "Gökçeada"
+  },
+  {
+    registration: "TC-JNR",
+    aircraft: "A330-343",
+    family: "A330-300",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: "Haliç Golden Horn"
+  },
 
-    {
-        registration: "TC-JNB",
-        aircraft: "Airbus A330-203",
-        family: "A330-200",
-        engine: "CF6-80E1A3",
-        manufacturer: "General Electric"
-    },
+  {
+    registration: "TC-JNS",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Hattuşaş"
+  },
+  {
+    registration: "TC-JNT",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Bursa"
+  },
+  {
+    registration: "TC-JNZ",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Kartalkaya"
+  },
+  {
+    registration: "TC-JOA",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Pamukkale"
+  },
+  {
+    registration: "TC-JOB",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Bozcaada"
+  },
+  {
+    registration: "TC-JOD",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Malazgirt"
+  },
+  {
+    registration: "TC-JOE",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Diyarbakır"
+  },
+  {
+    registration: "TC-JOF",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Sakarya"
+  },
+  {
+    registration: "TC-JOG",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Truva"
+  },
+  {
+    registration: "TC-JOH",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Sivas"
+  },
+  {
+    registration: "TC-JOI",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Kızılcahamam"
+  },
+  {
+    registration: "TC-JOJ",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Rize-Artvin"
+  },
+  {
+    registration: "TC-JOK",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: null
+  },
+  {
+    registration: "TC-JOL",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Aydın"
+  },
 
-    {
-        registration: "TC-JNC",
-        aircraft: "Airbus A330-203",
-        family: "A330-200",
-        engine: "CF6-80E1A3",
-        manufacturer: "General Electric"
-    },
+  {
+    registration: "TC-LNC",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Refahiye"
+  },
+  {
+    registration: "TC-LND",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: null
+  },
+  {
+    registration: "TC-LNE",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Göbeklitepe"
+  },
+  {
+    registration: "TC-LNF",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Kocaeli"
+  },
+  {
+    registration: "TC-LNG",
+    aircraft: "A330-303",
+    family: "A330-300",
+    engine: "CF6-80E1A3",
+    manufacturer: "General Electric",
+    name: "Mersin"
+  },
 
-    {
-        registration: "TC-JND",
-        aircraft: "Airbus A330-203",
-        family: "A330-200",
-        engine: "CF6-80E1A3",
-        manufacturer: "General Electric"
-    },
-
-    {
-        registration: "TC-JNE",
-        aircraft: "Airbus A330-203",
-        family: "A330-200",
-        engine: "CF6-80E1A3",
-        manufacturer: "General Electric"
-    },
-
-    {
-        registration: "TC-LOH",
-        aircraft: "Airbus A330-223",
-        family: "A330-200",
-        engine: "PW4168A",
-        manufacturer: "Pratt & Whitney"
-    },
-
-    {
-        registration: "TC-LOM",
-        aircraft: "Airbus A330-243",
-        family: "A330-200",
-        engine: "Trent 772B-60",
-        manufacturer: "Rolls-Royce"
-    },
-
-    {
-        registration: "TC-JNH",
-        aircraft: "Airbus A330-343",
-        family: "A330-300",
-        engine: "Trent 772B-60",
-        manufacturer: "Rolls-Royce"
-    },
-
-    {
-        registration: "TC-JNI",
-        aircraft: "Airbus A330-343",
-        family: "A330-300",
-        engine: "Trent 772B-60",
-        manufacturer: "Rolls-Royce"
-    },
-
-    {
-        registration: "TC-JNJ",
-        aircraft: "Airbus A330-343",
-        family: "A330-300",
-        engine: "Trent 772B-60",
-        manufacturer: "Rolls-Royce"
-    },
-
-    {
-        registration: "TC-JNK",
-        aircraft: "Airbus A330-343",
-        family: "A330-300",
-        engine: "Trent 772B-60",
-        manufacturer: "Rolls-Royce"
-    },
-
-    {
-        registration: "TC-JNL",
-        aircraft: "Airbus A330-343",
-        family: "A330-300",
-        engine: "Trent 772B-60",
-        manufacturer: "Rolls-Royce"
-    },
-
-    {
-        registration: "TC-JNM",
-        aircraft: "Airbus A330-343",
-        family: "A330-300",
-        engine: "Trent 772B-60",
-        manufacturer: "Rolls-Royce"
-    },
-
-    {
-        registration: "TC-JNN",
-        aircraft: "Airbus A330-343",
-        family: "A330-300",
-        engine: "Trent 772B-60",
-        manufacturer: "Rolls-Royce"
-    },
-
-    {
-        registration: "TC-JNO",
-        aircraft: "Airbus A330-343",
-        family: "A330-300",
-        engine: "Trent 772B-60",
-        manufacturer: "Rolls-Royce"
-    },
-
-    {
-        registration: "TC-JNP",
-        aircraft: "Airbus A330-343",
-        family: "A330-300",
-        engine: "Trent 772B-60",
-        manufacturer: "Rolls-Royce"
-    },
-
-    {
-        registration: "TC-JNR",
-        aircraft: "Airbus A330-343",
-        family: "A330-300",
-        engine: "Trent 772B-60",
-        manufacturer: "Rolls-Royce"
-    }
+  // Leased A330-300
+  {
+    registration: "TC-LOA",
+    aircraft: "A330-343",
+    family: "A330-300",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: null
+  },
+  {
+    registration: "TC-LOB",
+    aircraft: "A330-343",
+    family: "A330-300",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: null
+  },
+  {
+    registration: "TC-LOC",
+    aircraft: "A330-343",
+    family: "A330-300",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: null
+  },
+  {
+    registration: "TC-LOD",
+    aircraft: "A330-343",
+    family: "A330-300",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: null
+  },
+  {
+    registration: "TC-LOE",
+    aircraft: "A330-343",
+    family: "A330-300",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: null
+  },
+  {
+    registration: "TC-LOF",
+    aircraft: "A330-343",
+    family: "A330-300",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: null
+  },
+  {
+    registration: "TC-LOG",
+    aircraft: "A330-343",
+    family: "A330-300",
+    engine: "Trent 772B-60",
+    manufacturer: "Rolls-Royce",
+    name: null
+  },
+  {
+    registration: "TC-LON",
+    aircraft: "A330-302",
+    family: "A330-300",
+    engine: "CF6-80E1A4",
+    manufacturer: "General Electric",
+    name: null
+  }
 
 ];
-
-
-/* A330-300 / CF6 */
-
-[
-    "TC-JNS",
-    "TC-JNT",
-    "TC-JNZ",
-    "TC-JOA",
-    "TC-JOB",
-    "TC-JOD",
-    "TC-JOE",
-    "TC-JOF",
-    "TC-JOG",
-    "TC-JOH",
-    "TC-JOI",
-    "TC-JOJ",
-    "TC-JOK",
-    "TC-JOL",
-    "TC-LNC",
-    "TC-LND",
-    "TC-LNE",
-    "TC-LNF",
-    "TC-LNG"
-
-].forEach(registration => {
-
-    fleetData.push({
-
-        registration: registration,
-
-        aircraft: "Airbus A330-303",
-
-        family: "A330-300",
-
-        engine: "CF6-80E1A3",
-
-        manufacturer: "General Electric"
-
-    });
-
-});
-
-
-/* A330-300 / Trent */
-
-[
-    "TC-LOA",
-    "TC-LOB",
-    "TC-LOC",
-    "TC-LOD",
-    "TC-LOE",
-    "TC-LOF",
-    "TC-LOG"
-
-].forEach(registration => {
-
-    fleetData.push({
-
-        registration: registration,
-
-        aircraft: "Airbus A330-343",
-
-        family: "A330-300",
-
-        engine: "Trent 772B-60",
-
-        manufacturer: "Rolls-Royce"
-
-    });
-
-});
-
-
-/* A330-302 */
-
-fleetData.push({
-
-    registration: "TC-LON",
-
-    aircraft: "Airbus A330-302",
-
-    family: "A330-300",
-
-    engine: "CF6-80E1A4",
-
-    manufacturer: "General Electric"
-
-});
