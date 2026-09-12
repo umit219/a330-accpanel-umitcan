00cdcfbcf16309e836bb21c5c86f29bdd7ba0416
umit219 <157153442+umit219@users.noreply.github.com>
2026-09-12 20:55:17 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,110 +1,137 @@
-const CACHE = 'a330-v8';
+const CACHE = 'a330-v4';
 
 const FILES = [
-  '/',
-  '/index.html',
-  '/cb.html',
-  '/panels.html',
-  '/circuit.html',
-  '/sematik.html',
-  '/fleet.html',
-  '/unit.html',
-  '/inspection.html',
-  '/scanner.html',
-  '/theme.js',
-  '/data.js',
-  '/resetData.js',
-  '/fleet.js',
-  '/unit.js',
-  '/scanner.js',
-  '/inspection.js',
-  '/theme.css',
-  '/lh1.png',
-  '/rh2.png',
-  '/330.png',
-  '/kisaltmalar.html',
-  '/borescope.html',
-  '/reset.html',
-  '/bilgiler.html',
-  '/manifest.json'
+    '/',
+    '/index.html',
+
+    '/cb.html',
+    '/panels.html',
+    '/circuit.html',
+    '/sematik.html',
+
+    '/fleet.html',
+    '/unit.html',
+    '/inspection.html',
+
+    '/manhour.html',
+
+    '/theme.js',
+    '/data.js',
+    '/resetData.js',
+    '/fleet.js',
+    '/unit.js',
+    '/inspection.js',
+    '/manhour.js',
+
+    '/theme.css',
+
+    '/lh1.png',
+    '/rh2.png',
+    '/330.png',
+
+    '/kisaltmalar.html',
+    '/borescope.html',
+    '/reset.html',
+    '/bilgiler.html',
+
+    '/manifest.json'
 ];
 
 
-self.addEventListener('install', event => {
+self.addEventListener(
+    'install',
+    event => {
 
-  event.waitUntil(
+        event.waitUntil(
 
-    caches.open(CACHE)
-      .then(cache => cache.addAll(FILES))
-
-  );
-
-  self.skipWaiting();
-
-});
-
-
-self.addEventListener('activate', event => {
-
-  event.waitUntil(
-
-    caches.keys()
-      .then(keys => {
-
-        return Promise.all(
-
-          keys
-            .filter(key => key !== CACHE)
-            .map(key => caches.delete(key))
+            caches.open(CACHE)
+                .then(
+                    cache => cache.addAll(FILES)
+                )
+                .then(
+                    () => self.skipWaiting()
+                )
 
         );
 
-      })
-
-  );
-
-  self.clients.claim();
-
-});
+    }
+);
 
 
-self.addEventListener('fetch', event => {
+self.addEventListener(
+    'activate',
+    event => {
 
-  if (event.request.method !== 'GET') {
-    return;
-  }
+        event.waitUntil(
 
+            caches.keys()
+                .then(
+                    keys => {
 
-  event.respondWith(
+                        return Promise.all(
 
-    fetch(event.request)
+                            keys
+                                .filter(
+                                    key => key !== CACHE
+                                )
+                                .map(
+                                    key => caches.delete(key)
+                                )
 
-      .then(response => {
+                        );
 
-        const clone = response.clone();
+                    }
+                )
+                .then(
+                    () => self.clients.claim()
+                )
 
-        caches.open(CACHE)
-          .then(cache => {
-
-            cache.put(
-              event.request,
-              clone
-            );
-
-          });
-
-        return response;
-
-      })
-
-      .catch(() => {
-
-        return caches.match(
-          event.request
         );
 
-      })
+    }
+);
 
-  );
 
-});
+self.addEventListener(
+    'fetch',
+    event => {
+
+        event.respondWith(
+
+            fetch(event.request)
+                .then(
+                    response => {
+
+                        const copy =
+                            response.clone();
+
+                        caches.open(CACHE)
+                            .then(
+                                cache => {
+
+                                    cache.put(
+                                        event.request,
+                                        copy
+                                    );
+
+                                }
+                            );
+
+                        return response;
+
+                    }
+                )
+                .catch(
+                    () => {
+
+                        return caches.match(
+                            event.request
+                        );
+
+                    }
+                )
+
+        );
+
+    }
+);
